74a9d18e0c2808b22b77698c668ab540c14651a2
damettoluca <github@damettoluca.com>
2022-03-17 21:38:06 +0100

    Moved constants to consts.go for better organization

---
+++ consts.go
@@ -0,0 +1,7 @@
+package main
+
+const clientVersion = "1.0.0"
+const tdSessionFiles = "./DcCheckerSession"
+const backupFolder = tdSessionFiles + "/tmpDcStatus.json"
+const sessionFolder = tdSessionFiles + "/session.json"
+const apiEndpoint = "https://api.owlgram.org/"
--- main.go
+++ main.go
@@ -4,12 +4,6 @@ import (
 	"fmt"
 )
 
-const clientVersion = "1.0.0"
-const tdSessionFiles = "./DcCheckerSession"
-const backupFolder = tdSessionFiles + "/tmpDcStatus.json"
-const sessionFolder = tdSessionFiles + "/session.json"
-const apiEndpoint = "https://api.owlgram.org/"
-
 func main() {
 	fmt.Printf("TgServerChecker v%s, Copyright (C) 2021-2022 Laky-64 <https://github.com/Laky-64>\n"+
 		"Licensed under the terms of the GNU Lesser General Public License v3 or later (LGPLv3+)",
